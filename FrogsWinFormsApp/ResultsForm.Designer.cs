eb417a44d2f17c6b6dc220f47606583ec8e5ec1d
OstapRedneb <rodionzabirov888@gmail.com>
2025-08-20 17:30:08 +0500

    Сделал форму и код для реультатов

--- FrogsWinFormsApp/ResultsForm.Designer.cs
+++ FrogsWinFormsApp/ResultsForm.Designer.cs
@@ -28,12 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "ResultsForm";
+            showLabel = new Label();
+            resultsLabel = new Label();
+            exitButton = new Button();
+            deleteButton = new Button();
+            SuspendLayout();
+            // 
+            // showLabel
+            // 
+            showLabel.AutoSize = true;
+            showLabel.BackColor = Color.FromArgb(32, 32, 32);
+            showLabel.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            showLabel.ForeColor = Color.FromArgb(128, 255, 128);
+            showLabel.Location = new Point(12, 9);
+            showLabel.Name = "showLabel";
+            showLabel.Size = new Size(107, 23);
+            showLabel.TabIndex = 0;
+            showLabel.Text = "Результаты:";
+            // 
+            // resultsLabel
+            // 
+            resultsLabel.AutoSize = true;
+            resultsLabel.BackColor = Color.FromArgb(32, 32, 32);
+            resultsLabel.Font = new Font("Comic Sans MS", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            resultsLabel.ForeColor = Color.FromArgb(128, 255, 128);
+            resultsLabel.Location = new Point(12, 39);
+            resultsLabel.Name = "resultsLabel";
+            resultsLabel.Size = new Size(0, 23);
+            resultsLabel.TabIndex = 1;
+            // 
+            // exitButton
+            // 
+            exitButton.BackColor = Color.FromArgb(32, 32, 32);
+            exitButton.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            exitButton.ForeColor = Color.SpringGreen;
+            exitButton.Location = new Point(632, 378);
+            exitButton.Name = "exitButton";
+            exitButton.Size = new Size(156, 60);
+            exitButton.TabIndex = 4;
+            exitButton.Text = "Назад";
+            exitButton.UseVisualStyleBackColor = false;
+            exitButton.Click += exitButton_Click;
+            // 
+            // deleteButton
+            // 
+            deleteButton.BackColor = Color.FromArgb(32, 32, 32);
+            deleteButton.Font = new Font("Comic Sans MS", 18F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            deleteButton.ForeColor = Color.Brown;
+            deleteButton.Location = new Point(276, 378);
+            deleteButton.Name = "deleteButton";
+            deleteButton.Size = new Size(298, 60);
+            deleteButton.TabIndex = 5;
+            deleteButton.Text = "Удалить результаты";
+            deleteButton.UseVisualStyleBackColor = false;
+            deleteButton.Click += deleteButton_Click;
+            // 
+            // ResultsForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources._9dec81c3ff0d412ce22829d301b0dcf8;
+            BackgroundImageLayout = ImageLayout.Stretch;
+            ClientSize = new Size(800, 450);
+            Controls.Add(deleteButton);
+            Controls.Add(exitButton);
+            Controls.Add(resultsLabel);
+            Controls.Add(showLabel);
+            Name = "ResultsForm";
+            Text = "ResultsForm";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label showLabel;
+        private Label resultsLabel;
+        private Button exitButton;
+        private Button deleteButton;
     }
 }
--- FrogsWinFormsApp/ResultsForm.cs
+++ FrogsWinFormsApp/ResultsForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.VisualBasic.ApplicationServices;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using _2048ClassLibrary;
+using User = _2048ClassLibrary.User;
 
 namespace FrogsWinFormsApp
 {
@@ -15,6 +18,28 @@ namespace FrogsWinFormsApp
         public ResultsForm()
         {
             InitializeComponent();
+            List<User> users = FileWorker.GetUsersList();
+            users.Sort();
+
+            resultsLabel.Text = string.Join("\n", users);
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show("Вы уверены что хотите удалить все реультаты?\nВсе данные будут безвозвратно утеряны!", "Предупреждение", MessageBoxButtons.OKCancel, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.OK)
+            {
+                FileWorker.Clear();
+
+                resultsLabel.Text = "";
+            }
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            new MenuForm().Show();
+            Hide();
         }
     }
 }
